d9aacce38803b1e6b1889f309412faa9d3a1b2f5
Ceyhun <50988930+Alacadrial@users.noreply.github.com>
2023-03-31 16:21:35 +0300

    initial upload.

---
+++ controller.py
@@ -0,0 +1,282 @@
+from voicevox import Client
+import re
+import asyncio
+from PIL import Image
+import cv2
+import pyautogui
+import pytesseract
+import numpy as np
+import simpleaudio as sa
+from pynput import mouse
+import time
+from io import BytesIO
+import base64
+import pickle
+from translate import Translator
+import os
+import shutil
+
+
+class ROI:
+
+	def __init__(self):
+		self.ix, self.iy, self.ex, self.ey = -1, -1, -1 ,-1;
+
+	def listen_mouse(self):
+		with mouse.Listener(on_click=self.on_click) as listener:
+			listener.join()
+
+	# Maybe use this to draw a rectangle on screen. Not used
+	def on_move(self, x, y):
+		if self.ix is not None and self.iy is not None:
+			self.ex = x
+			self.ey = y
+			print(f"ROI coordinates: ({self.ix}, {self.iy}) - ({self.ex}, {self.ey})")
+
+	def on_click(self, x, y, button, pressed):
+		if pressed:
+			if button == mouse.Button.left:
+				self.ix, self.iy = x, y
+		else:
+			if button == mouse.Button.left:
+				self.ex, self.ey = x, y
+				return False
+
+	def get_roi(self):
+		return (min(self.ix, self.ex), min(self.iy, self.ey), abs(self.ix-self.ex), abs(self.iy-self.ey))
+
+
+class VnvController:
+
+	def __init__(self):
+		self.client = Client()
+		self.translator = Translator(to_lang="ja")
+		self.roi = ROI()
+		self.screenshot = None
+		self.text_roi = None    
+		self.name_roi = None
+		self.last_valid_query = None  # (id, text), this will play when we press play hotkey
+		self.voice_dict = dict()
+		self.audio_cache = dict()
+
+		self.history = None  # (name, text)
+		self.error = "" 
+
+		self.current_name = "Self/Narrator"
+		self.auto_play = False
+		self.translate = False 								
+		
+		self.fetching_queries = list()
+		self.play_object = None
+		self.audio_tasks = list()
+
+		self.set_tesseract_path()
+		# zodiac's tesseract model is really good at detecting japanese text.
+		self.lang_pack = 'eng+jpn+jpn_zodiac3539_ver5'
+
+	async def get_speakers(self):
+		speaker_dict = dict()
+		speakers = await self.client.fetch_speakers()
+		for speaker in speakers:
+			speaker_info = await self.client.fetch_speaker_info(speaker.uuid)
+			if speaker_info:
+				speaker_dict[speaker.uuid] = dict()
+				speaker_dict[speaker.uuid]["name"] = speaker.name 
+				speaker_dict[speaker.uuid]["icon"] = BytesIO(base64.b64decode(speaker_info.style_infos[0].icon))
+				speaker_dict[speaker.uuid]["styles"] = [[style.id, style.name] for style in speaker.styles]
+				# Contains Voice Samples For Each Variant
+				speaker_dict[speaker.uuid]["samples"] = dict() 
+				for style in speaker_info.style_infos:
+					speaker_dict[speaker.uuid]["samples"][style.id] = [BytesIO(base64.b64decode(sample)).getvalue() for sample in style.voice_samples]
+		return speaker_dict
+
+	async def close(self):
+		await self.client.close()
+
+	def set_tesseract_path(self):
+		tesseract_cmd = 'tesseract'
+		if os.name == 'nt':  # Windows
+			tesseract_cmd = 'tesseract.exe'
+
+		# Search for tesseract in the system path
+		for path in os.environ["PATH"].split(os.pathsep):
+			path = path.strip('"')
+			executable = os.path.join(path, tesseract_cmd)
+			if os.path.isfile(executable):
+				# Found tesseract, set the path and exit the loop
+				pytesseract.pytesseract.tesseract_cmd = executable
+				break
+		else:
+				# tesseract not found in the system path
+				print("Error: tesseract not found in the system path")
+				exit(-1)
+
+	def save_profile(self, path):
+		profile = {
+			"name_roi" : self.name_roi,
+			"text_roi" : self.text_roi,
+			"voice_dict" : self.voice_dict,
+			"auto_play" : self.auto_play,
+			"translate" : self.translate
+		}
+		print("Saving Profile: ")
+		with open(path, "wb") as file:
+			pickle.dump(profile, file)
+
+	def load_profile(self, path):
+		print("Loading Profile: ")
+		with open(path, "rb") as file:
+			profile = pickle.load(file)
+		self.name_roi = profile["name_roi"]
+		self.text_roi = profile["text_roi"]
+		self.voice_dict = profile["voice_dict"]
+		self.auto_play = profile["auto_play"]
+		self.translate = profile["translate"]
+
+	def assign_voice(self, name, speaker_id):
+		self.voice_dict[name] = speaker_id
+
+	def set_auto_play(self, value):
+		self.auto_play = value
+
+	def set_translate(self, value):
+		self.translate = value
+
+	def set_text_roi(self, roi):
+		self.text_roi = roi
+
+	def set_name_roi(self, roi):
+		self.name_roi = roi
+
+	def select_roi(self):
+		self.roi.listen_mouse()
+		return self.roi.get_roi()
+
+	def preprocess_english(self, text):
+		return text
+
+	def preprocess_japanese(self, text):
+		# Remove spaces
+		text = text.replace(" ", "").strip()
+		# Remove non-ASCII characters except punctuation, ASCII letters, Hiragana, Katakana, and Kanji characters
+		text = re.sub(r'[^\x00-\x7F\u3000-\u30FF\uFF00-\uFFEF\u3040-\u309F\u30A0-\u30FF\u4E00-\u9FBF]', '', text)
+		return text
+
+	def get_text_from_roi(self, roi):
+
+		if roi == None:
+			return ""
+
+		image = pyautogui.screenshot(region=roi)
+
+		# Convert Gray
+		image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+
+		# Optimization of Accuracy, really effective.
+		image = cv2.GaussianBlur(image, (5, 5), 0) 
+
+		# Perform OCR, Takes the longest in computation time.
+		text = pytesseract.image_to_string(image, lang=self.lang_pack)
+
+		# Final process of the text. Lookup table for functions if more languages are added.
+		preprocess_text = self.preprocess_english if self.translate else self.preprocess_japanese
+		return preprocess_text(text)	
+
+	async def screen_capture_loop(self):
+		print("Screen Capture Loop Task Started")
+		previous_text = ""
+		found_new_valid_query = False
+
+		print("Waiting text roi to be set.")
+		# Wait for text roi to be set.
+		while True:
+			if self.text_roi == None:
+				await asyncio.sleep(0.5)
+				continue
+			break
+		
+		print("Text roi is set, starting screencapture.")
+		# Capture Loop
+		while True:
+			start = time.time()
+			
+			name = self.get_text_from_roi(self.name_roi).strip()
+			text = self.get_text_from_roi(self.text_roi).strip()
+
+			if not name:
+				name = "Self/Narrator"
+
+			self.current_name = name
+			
+			if text == "":
+				print("No text, therefore no query.")
+				continue
+
+			if name not in self.voice_dict:
+				print("Name is not assigned to a voice therefore no query.")
+				continue
+
+			speaker = self.voice_dict[name]
+
+			if previous_text == text:
+				self.last_valid_query = (speaker, text)
+				# First time seeing this valid Query. Therefore initilizing history append and maybe audio play.
+				if not found_new_valid_query:
+					self.history = (name, text)
+					found_new_valid_query = True
+					if self.auto_play:
+						print("Starting audio task from auto-play.")
+						asyncio.create_task(self.play_audio_using_query(self.last_valid_query))
+			else:
+				# Change in text, reset variables
+				found_new_valid_query = False	
+			
+			previous_text = text
+			print("Scan Iteration Took: ", time.time() - start)		
+			await asyncio.sleep(0.1)
+	
+	async def play_audio_using_query(self, query):
+		speaker_id, text = query
+
+		if query in list(self.audio_cache.keys()):
+			audio_data = self.audio_cache[query]
+		else:
+			passing_argument = (speaker_id, text)
+
+			if self.translate:
+				passing_argument = (speaker_id, self.translator.translate(text))
+
+			if passing_argument in self.fetching_queries:
+				print("A task is already assigned to synthesise audio from this query, returning.")
+				return
+			
+			self.fetching_queries.append(passing_argument)
+			audio_data = await self.synthesise_audio(passing_argument)
+			self.fetching_queries.remove(passing_argument)
+		
+		self.audio_cache[query] = audio_data
+		asyncio.create_task(self.play_audio(audio_data))
+		
+	async def synthesise_audio(self, query):
+		start = time.time()
+		speaker, text = query
+		audio_query = await self.client.create_audio_query(text, speaker=speaker)
+		audio_query.volume_scale = 2.0
+		audio_data = await audio_query.synthesis(speaker=speaker)
+		print("Synthesis Request Took: ", time.time() - start)
+		return audio_data
+
+	async def play_audio(self, audio_data):
+		# save audio to file.
+		with open("voice.wav", "wb") as f:
+			f.write(audio_data)
+
+		# interrupt already playing audio.
+		if self.play_object is not None and self.play_object.is_playing():
+			self.play_object.stop()
+
+		# Play the new audio.
+		self.wave_object = sa.WaveObject.from_wave_file("voice.wav")
+		self.play_object = self.wave_object.play()
+
+
---
+++ main.py
@@ -0,0 +1,228 @@
+from controller import VnvController
+import asyncio
+import threading as th
+import dearpygui.dearpygui as dpg
+from PIL import Image
+import platform
+
+
+def task_thread(coro, loop):
+	print("run_coroutine_threadsafe")
+	future = asyncio.run_coroutine_threadsafe(coro, loop)
+	loop.run_forever()
+
+def prompt_about(sender, app_data, user_data):
+	window_id = user_data[0]
+	try:
+		with dpg.window(label="About", tag="about_window", pos=[0, 0], min_size=[350,200], on_close=lambda: dpg.remove_alias("about_window")) as window:
+			dpg.add_text("Thank you for trying my application.")
+			dpg.add_text("For usage check out my github repository.")
+			with dpg.group(horizontal=True):
+				dpg.add_text("Repo: ")
+				dpg.add_input_text(label="##", default_value="https://github.com/Alacadrial/visual-novel-voicer",readonly=True)
+	except Exception as err:
+		print(err)
+		pass
+
+def prompt_file_selector(sender, app_data, user_data):
+	label, callback, controller = user_data 
+	with dpg.file_dialog(label=label, callback=callback, user_data=[controller], min_size=[350,400], max_size=[350,400], default_path="./profiles/"):
+		dpg.add_file_extension(".profile")
+
+def save_profile(sender, app_data, user_data):
+	controller = user_data[0]
+	controller.save_profile(app_data["file_path_name"])
+	# File Name
+	dpg.set_value("log_text", "Profile Save")
+
+def load_profile(sender, app_data, user_data):
+	controller = user_data[0]
+	controller.load_profile(app_data["file_path_name"])
+	dpg.set_value("log_text", "Profile Load")
+	dpg.set_value("checkbox_auto_play", controller.auto_play)
+	dpg.set_value("checkbox_translate", controller.translate)
+
+def assign_bool(sender, app_data, user_data):
+	setter_function = user_data[0]
+	value = dpg.get_value(sender)
+	setter_function(value)
+
+def assign_voice(sender, app_data, user_data):
+	controller = user_data[0]
+	name = dpg.get_value("current_name")
+	uuid = sender.split("_")[1]
+	speaker_id = int(dpg.get_value(f"variant_{uuid}").split(":")[1])
+	controller.assign_voice(name, speaker_id)
+	dpg.set_value("log_text", f"Assigned '{name}' to [{speaker_id}]")
+
+def assign_roi(sender, app_data, user_data):
+	dpg.set_value("log_text", "Listening Mouse... Drag&Drop")
+	select_roi, set_roi = user_data
+	roi = select_roi()
+	set_roi(roi)
+	dpg.set_value("log_text", "--")
+
+def listen_last_query_on_key_press(controller, loop):
+	loop.create_task(controller.play_audio_using_query(query=controller.last_valid_query))
+	dpg.set_value("log_text", f"Listening last valid query.")
+
+def listen_sample(sender, app_data, user_data):
+	controller, speakers, loop = user_data
+	uuid = sender.split("_")[1]
+	speaker_id = int(dpg.get_value(f"variant_{uuid}").split(":")[1])
+	# normally a list that contains multiple samples, but working with the introduction sample for now.
+	audio_data = speakers[uuid]["samples"][speaker_id][0] 
+	loop.create_task(controller.play_audio(audio_data))
+	dpg.set_value("log_text", f"Listening sample voice for [{speaker_id}]")
+
+def listen_entry(sender, app_data, user_data):
+	controller, loop = user_data
+	entry_index = sender.split("_")[-1]
+	name = dpg.get_value(f"history_entry_name_{entry_index}").strip()
+	text = dpg.get_value(f"history_entry_text_{entry_index}").strip()
+	speaker_id = controller.voice_dict[name]
+	query = (speaker_id, text)
+	loop.create_task(controller.play_audio_using_query(query))
+	dpg.set_value("log_text", f"Listening entry for [{name}, {entry_index}]")
+
+
+async def main():
+	controller = VnvController()
+	main_event_loop = asyncio.get_event_loop()		# Is used to create tasks to play sample voices.
+	
+	speakers = await controller.get_speakers()
+	speaker_uuids = list(speakers.keys())
+
+	dpg.create_context()
+	dpg.create_viewport(title='VNV', width=350, height=510, resizable=False)
+	
+	# registery will be done in loop then when we create window we will do another loop to create components.
+	texture_list = list()
+	with dpg.texture_registry():
+		for speaker in speakers: 
+			image = Image.open(speakers[speaker]["icon"])
+			image.save("icon.png")
+			# Sadly there is no way to load image from buffer.
+			width, height, channels, data = dpg.load_image("icon.png")
+			texture_id = dpg.add_static_texture(width, height, data)
+			texture_list.append(texture_id)
+
+	# Register font that works with japanese and english.
+	with dpg.font_registry():
+		font_path = "./fonts/MSGOTHIC.TTF"
+		with dpg.font(font_path, 13) as font1:
+			dpg.add_font_range_hint(dpg.mvFontRangeHint_Japanese)
+			dpg.bind_font(font1)
+
+	# Press F9 to play last valid query
+	# For Windows and macOS, linux requires admin rights and when ran as root, it had trouble PCM device to play audio.
+	if platform.system() in ["Windows", "Darwin"]:
+		import keyboard
+		keyboard.add_hotkey("f9", lambda: listen_last_query_on_key_press(controller, main_event_loop))
+
+	#This only works when dearpygui process is focused. Therefore I have decided not to use it. Instead keyboard module is used.
+	#with dpg.handler_registry():
+   	#	dpg.add_key_press_handler(dpg.mvKey_F9, callback=listen_last_query_on_key_press, user_data=[controller, main_event_loop])
+
+	# Start Screen Capture Thread
+	capture_event_loop = asyncio.new_event_loop()	# Constantly running loop on another thread to capture screen.
+	capture_thread = th.Thread(target=task_thread, args=(controller.screen_capture_loop(), capture_event_loop))
+	capture_thread.daemon = True
+	capture_thread.start()
+
+	#Primary Window, There will be two tabs under this later.
+	with dpg.window(label="Primary Window", no_resize=True, max_size=[350, 400]) as primary_window:
+		primary_window_id = primary_window
+		with dpg.menu_bar():
+			with dpg.menu(label="Profile"):
+				dpg.add_menu_item(label="Load", callback=prompt_file_selector, user_data=["Load Profile File", load_profile, controller])
+				# dpg.add_menu_item(label="Save")
+				dpg.add_menu_item(label="Save As", callback=prompt_file_selector, user_data=["Save Profile File", save_profile, controller])
+			dpg.add_menu_item(label="About", callback=prompt_about, user_data=[primary_window_id])
+
+		with dpg.tab_bar():
+			with dpg.tab(label="Speakers"):
+				# Group Main Div For Speaker Components
+				with dpg.group():
+					icon_width, icon_height = 85, 85
+					split_list = [texture_list[i:i+3] for i in range(0, len(texture_list), 3)]
+					index = 0
+					# For each 3 texture create a Row
+					for part in split_list:
+						# Row
+						with dpg.group(horizontal=True):
+							for texture_id in part:
+								# Component
+								with dpg.group():
+									# Current Speaker's Values for GUI
+									uuid = speaker_uuids[index]
+									styles = speakers[uuid]["styles"]
+									combo_items = [f"{name}:{_id}" for _id, name in styles]
+									enabled = False if len(styles) == 1 else True
+
+									# Icon
+									dpg.add_image(texture_id, width=icon_width, height=icon_height)
+									# Line where you listen to sample and select variations.
+									with dpg.group(horizontal=True, horizontal_spacing=0):
+										button_width = round(icon_width / 3)
+										dpg.add_button(tag=f"listen_{uuid}", label="L", callback=listen_sample, user_data=[controller, speakers, main_event_loop], width=button_width)
+										# if style_infos == 1 disabled combobox with already selected ID
+										dpg.add_combo(tag=f"variant_{uuid}", items=combo_items, default_value=combo_items[0], enabled=enabled, no_arrow_button=True, width=icon_width-button_width)
+									dpg.add_button(tag=f"assign_{uuid}", label="Assign Voice", callback=assign_voice, user_data=[controller], width=icon_width)
+									index += 1
+			with dpg.tab(tag="tab_history", label="History"):
+				last_history_entry = 0 # Holds ID of last entry component
+				history_entry_index = 0 # For uuid of a entry to access them from button press.
+				# Text Capture History Will be here, updated dynamically on each render loop.
+				pass
+
+	with dpg.window(label="areaSelect", no_resize=True, no_title_bar=True, no_move=True, min_size=[350,100], pos=[0,400]):
+		with dpg.group(horizontal=True):
+			dpg.add_text("Current Name:   ")
+			dpg.add_input_text(label="##", tag="current_name", readonly=True)
+		with dpg.group(horizontal=True):
+			dpg.add_text("Select Regions: ")
+			dpg.add_button(tag="button_nameroi", label="Character Name", callback=assign_roi, user_data=[controller.select_roi, controller.set_name_roi], width=100, height=20)
+			dpg.add_button(tag="button_textroi", label="Text", callback=assign_roi, user_data=[controller.select_roi, controller.set_text_roi], width=60, height=20)
+		with dpg.group(horizontal=True):
+			dpg.add_text("Autoplay Audio: ")
+			dpg.add_checkbox(tag="checkbox_auto_play", callback=assign_bool, user_data=[controller.set_auto_play])
+			dpg.add_text("Translate to Japanese: ")
+			dpg.add_checkbox(tag="checkbox_translate", callback=assign_bool, user_data=[controller.set_translate])
+		with dpg.group(horizontal=True):
+			dpg.add_text("Log: ")
+			dpg.add_input_text(label="##", tag="log_text", default_value="--", readonly=True)
+
+	dpg.setup_dearpygui()
+	dpg.set_primary_window(primary_window_id, True)
+	dpg.show_viewport()
+
+	# below replaces, start_dearpygui()
+	while dpg.is_dearpygui_running():
+	# insert here any code you would like to run in the render loop
+	# you can manually stop by using stop_dearpygui()
+		dpg.set_value("current_name", controller.current_name)
+		
+		# Show Error
+		if controller.error:
+			dpg.set_value("log_text", f"error: {controller.error}")
+
+		# Insert to History Tab
+		if controller.history:
+			name, text = controller.history
+			with dpg.group(parent="tab_history", before=last_history_entry) as grp:
+				with dpg.group(horizontal=True):
+					dpg.add_text(name + " "*(20-len(name)), tag=f"history_entry_name_{history_entry_index}")
+					dpg.add_button(label="L", width=28, tag=f"listen_history_entry_{history_entry_index}", callback=listen_entry, user_data=[controller, main_event_loop])            
+				dpg.add_text(text, tag=f"history_entry_text_{history_entry_index}")
+				last_history_entry = grp
+				history_entry_index += 1
+			controller.history = None
+
+		dpg.render_dearpygui_frame()
+		await asyncio.sleep(0.1)
+
+	dpg.destroy_context()
+
+if __name__ == '__main__':
+	asyncio.run(main())
